b5667a527cdd8472c9449cc8773c58a21647e31a
Eugenio Parodi <ceccopierangiolieugenio@googlemail.com>
2015-01-16 17:33:36 +0000

    Revert "#34 3D Game Engine Tutorial: Scene Graph"
    This reverts commit a8d3eb3567ed63b31910511ceadbef29541cfff5.

--- js/game/TestGame.js
+++ js/game/TestGame.js
@@ -20,9 +20,46 @@ function TestGame() {
 
 TestGame.prototype.init = function ()
 {
-    this.root = new GameObject();
+    //ResourceLoader.loadMesh("box.obj");
+    this.material = new Material(new Texture("test.png"), new Vector3f(1, 1, 1), 1, 8);
+    this.shader = BasicShader.getInstance();//this.shader = PhongShader.getInstance();
     this.camera = new Camera();
- 
+    this.transform = new Transform();
+
+    this.pLight1 = new PointLight(new BaseLight(new Vector3f(1, 0.5, 0), 0.8), new Attenuation(0, 0, 1), new Vector3f(-2, 0, 5), 10);
+    this.pLight2 = new PointLight(new BaseLight(new Vector3f(0, 0.5, 1), 0.8), new Attenuation(0, 0, 1), new Vector3f(2, 0, 7), 10);
+
+    this.sLight1 = new SpotLight(new PointLight(new BaseLight(new Vector3f(0, 1, 1), 0.8), new Attenuation(0, 0, 0.1), new Vector3f(-2, 0, 5), 30), new Vector3f(1, 1, 1), 0.7);
+
+    this.temp = 0.0;
+
+//    var vertices = [
+//        new Vertex(new Vector3f(-1, -1, 0), new Vector2f(0, 0)),
+//        new Vertex(new Vector3f(0, 1, 0), new Vector2f(0.5, 0)),
+//        new Vertex(new Vector3f(1, -1, 0), new Vector2f(1.0, 0)),
+//        new Vertex(new Vector3f(0, -1, 1), new Vector2f(0.5, 1.0))
+//    ];
+//
+//    var indices = [
+//        3, 1, 0,
+//        2, 1, 3,
+//        0, 1, 2,
+//        0, 2, 3
+//    ];
+
+//    var vertices = [
+//        new Vertex(new Vector3f(-1.0, -1.0, 0.5773), new Vector2f(0.0, 0.0)),
+//        new Vertex(new Vector3f(0.0, -1.0, -1.15475), new Vector2f(0.5, 0.0)),
+//        new Vertex(new Vector3f(1.0, -1.0, 0.5773), new Vector2f(1.0, 0.0)),
+//        new Vertex(new Vector3f(0.0, 1.0, 0.0), new Vector2f(0.5, 1.0))
+//    ];
+//
+//    var indices = [
+//        0, 3, 1,
+//        1, 3, 2,
+//        2, 3, 0,
+//        1, 2, 0];
+
     var fieldDepth = 10.0;
     var fieldWidth = 10.0;
 
@@ -38,29 +75,61 @@ TestGame.prototype.init = function ()
         2, 1, 3
     ];
 
-    var mesh = new Mesh(vertices, indices, true);
-    var material = new Material(new Texture("test.png"), new Vector3f(1, 1, 1), 1, 8);
-
-    var meshRenderer = new MeshRenderer(mesh, material);
-    this.root.addComponent(meshRenderer);
+    this.mesh = new Mesh(vertices, indices, true);
+    /* NOTE (Eugenio): Addition to fix a problem with the Shader Attrib */
+    this.mesh.setShader(this.shader);
 
     Transform.setProjection(70, gl.viewportWidth, gl.viewportHeight, 0.1, 1000);
     Transform.setCamera(this.camera);
+
+//    PhongShader.setAmbientLight(new Vector3f(0.1, 0.1, 0.1));
+//    PhongShader.setDirectionalLight(new DirectionalLight(new BaseLight(new Vector3f(1, 1, 1), 0.1), new Vector3f(1, 1, 1)));
+//    
+//    PhongShader.setPointLight([this.pLight1, this.pLight2]);
+//    PhongShader.setSpotLights([this.sLight1]);
+
+    /* Added a "bind()" here to avoid the warning:
+     *       WebGL: UniformXXX: no program is currently bound
+     * caused by calling any Uniform function before the first frame.
+     */
+    this.shader.bind();
 };
 
 TestGame.prototype.input = function ()
 {
     this.camera.input();
-    this.root.input();
+//    if (Input.getKeyDown(Input.KEY_UP))
+//        console.log("We've just pressed up!");
+//    if (Input.getKeyUp(Input.KEY_UP))
+//        console.log("We've just released up!");
+//
+//    if (Input.getMouseDown(1))
+//        console.log("We've just right clicked at " + Input.getMousePosition().toString());
+//    if (Input.getMouseUp(1))
+//        console.log("We've just released right mouse button!");
 };
 
 TestGame.prototype.update = function ()
 {
-    this.root.getTransform().setTranslation(0, -1, 5);
-    this.root.update();
+    this.temp += Time.getDelta();
+
+    var sinTemp = Math.sin(this.temp);
+
+    this.transform.setTranslation(0, -1, 5);
+    //this.transform.setRotation(0, sinTemp * 180, 0);
+
+    this.pLight1.setPosition(new Vector3f(3, 0, 8.0 * (Math.sin(this.temp) + 1.0 / 2.0) + 10));
+    this.pLight2.setPosition(new Vector3f(7, 0, 8.0 * (Math.cos(this.temp) + 1.0 / 2.0) + 10));
+
+    //this.transform.setScale(0.7 * sinTemp, 0.7 * sinTemp, 0.7 * sinTemp);
+    this.sLight1.getPointLight().setPosition(this.camera.getPos());
+    this.sLight1.setDirection(this.camera.getForward());
 };
 
 TestGame.prototype.render = function ()
 {
-    this.root.render();
+    RenderUtil.setClearColor(Transform.getCamera().getPos().div(2048).abs());
+    this.shader.bind();
+    this.shader.updateUniforms(this.transform.getTransformation(), this.transform.getProjectedTransformation(), this.material);
+    this.mesh.draw();
 };
